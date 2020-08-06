0296d8fecaf65ac88c66840d2ae777dbca4a9cd2
filip.tyborowski <filip.tyborowski@gmail.com>
2020-08-06 16:21:21 +0200

    gates generates and we can see them

--- cars.py
+++ cars.py
@@ -3,6 +3,7 @@ import neat
 import time
 import os
 import math
+from itertools import product
 pygame.font.init()
 
 # load images
@@ -11,6 +12,7 @@ CARS = pygame.image.load(os.path.join("imgs", "cars.png"))
 
 WIN_WIDTH = BG_IMG.get_width()
 WIN_HEIGHT = BG_IMG.get_height()
+RADIUS_LENGTH = 190
 
 STAT_FONT = pygame.font.SysFont('comicsans', 40)
 
@@ -18,7 +20,7 @@ DEBUG = True
 
 
 class Car:
-    def __init__(self, carsIMG, i=0):
+    def __init__(self, carsIMG, i=0, gates=[]):
         # self.offset_x1 = 2
         # self.offset_y1 = 20
         # self.offset_x2 = 4
@@ -28,13 +30,13 @@ class Car:
 
         # self.rect = pygame.Rect(self.offset_x1 + self.car_number * (self.offset_x2 + self.car_widht),
         #                         self.offset_y1 + self.cars_row*(self.offset_y2 + self.car_height), self.car_widht, self.car_height)
-
         self.offset_x1 = 20
         self.offset_x2 = 23
         self.offset_y1 = 2
         self.offset_y2 = 4
         self.car_widht = 83
         self.car_height = 58
+        self.gates = gates
 
         self.cars_row = 0
         self.cars_col = 0
@@ -72,6 +74,10 @@ class Car:
         self.distance = 0
         self.time_spent = 0
         self.MAX_SPEED = 25
+        self.next_gate = 0
+
+    def get_current_gate(self):
+        return self.gates[self.next_gate]
 
     def speedUp(self):
         self.speed += 1
@@ -111,12 +117,25 @@ class Car:
 
     def draw_radar(self, win):
 
+        txt_lap = STAT_FONT.render(
+            f"Lap: 0/0", 1, (255, 0, 0))
+
+        txt_gate = STAT_FONT.render(
+            f"Gate: {self.next_gate}/{len(self.gates)}", 1, (255, 0, 0))
+
+        win.blit(txt_lap, (WIN_WIDTH - 150, 10))
+        win.blit(txt_gate, (WIN_WIDTH - 150, 40))
+
         for i, r in enumerate(self.radars):
             pos, dist = r
             pygame.draw.line(win, (0, 255, 0), self.center, pos, 1)
             pygame.draw.circle(win, (0, 255, 0), pos, 5)
             text = STAT_FONT.render(f"{i}: {dist}", 1, (255, 255, 255))
-            win.blit(text, (WIN_WIDTH - 100, 10 + i * 30))
+            win.blit(text, (WIN_WIDTH - 150, 70 + i * 30))
+
+        gt = self.get_current_gate()
+        pygame.draw.line(win, (0, 255, 255), self.center, gt[0], 1)
+        pygame.draw.line(win, (0, 255, 255), self.center, gt[1], 1)
 
     def check_radar(self, degree):
         len = 0
@@ -166,19 +185,73 @@ class GatesGenerator:
         self.number_of_gates = number_of_gates
         self.gates = []
 
+    def getAllGates(self):
+        return self.gates
+
+    def getGatesToPass(self):
+        gates = []
+
+        for g in self.getAllGates():
+            gates.append(g.getGate())
+
+        return gates
+
     def generate(self):
-        for g in range(1, self.number_of_gates):
-            gate = Gate(self.start_pos, self.background)
-            # Use max sourdund to find x,y for next gate!!!
+        keepCreating = True
+        i = 0
+        while keepCreating:
+            # for i in range(0, self.number_of_gates):
+            gate = Gate(-1)
+            if i > 0:
+                gate = Gate(i, self.gates[i - 1].getNextPoint(),
+                            self.background, self.gates[i - 1].center_pos, self.start_pos)
+
+                keepCreating = self.outsideRange(
+                    self.gates[i - 1].getNextPoint(), self.start_pos)
+            else:
+                gate = Gate(i, self.start_pos, self.background)
+
+            gate.calculatePos()
+            if i == 1:
+                self.start_pos = self.gates[i - 1].center_pos
+
+            self.gates.append(gate)
+            i += 1
+
+            keepCreating = not gate.finished
+
+    def outsideRange(self, point, circleXY, radius=125):
+        x1, y1 = point
+        x, y = circleXY
+        return math.pow(x1 - x, 2) + math.pow(y1 - y, 2) > math.pow(radius, 2)
 
 
 class Gate:
-    def __init__(self, pos, background):
+    def __init__(self, number, pos=(0, 0), background="NONE", prev_pos=(0.0, 0.0), meta=(0, 0)):
+        self.number = number
         self.pos = pos
-        self.track_color = background.get_at(self.pos)
+        self.prev_pos = prev_pos
         self.background = background
+        self.track_color = (0, 0, 0)
         self.surround = []
+        self.center_pos = (0, 0)
+        self.maxDist = ((0, 0), 0)
+        self.meta = meta
+        self.finished = False
+
+    def calculatePos(self):
+        self.track_color = self.background.get_at(self.pos)
         self.center_pos = self.center()
+        self.CalcMaxDist()
+
+        dist = math.sqrt(
+            math.pow(self.meta[0] - self.center_pos[0], 2) + math.pow(self.meta[1] - self.center_pos[1], 2))
+
+        if dist <= (RADIUS_LENGTH - 20):
+            self.finished = True
+
+    def canContinue(self):
+        return self.maxDist[1][0] != 0
 
     def center(self):
         self.surround.clear()
@@ -190,19 +263,128 @@ class Gate:
         for i, s in enumerate(self.surround):
             if s[1] == md:
                 self.minDist = s
-                self.maxDist = self.surround[(i + 18) % 36]
+                startAngle = (i + 14) % 36
+                endAngle = (startAngle + 9) % 36
 
-        return (int((self.minDist[0][0] + self.maxDist[0][0])/2), int((self.minDist[0][1] + self.maxDist[0][1])/2))
+                minDistOpposite = RADIUS_LENGTH
+                while startAngle % 36 != endAngle % 36:
+                    if minDistOpposite > self.surround[startAngle % 36][1]:
+                        self.minDist180 = self.surround[startAngle % 36]
+                        minDistOpposite = self.surround[startAngle % 36][1]
+                    startAngle = (startAngle + 1) % 36
+
+        return (int((self.minDist[0][0] + self.minDist180[0][0])/2), int((self.minDist[0][1] + self.minDist180[0][1])/2))
+
+    def CalcMaxDist(self):
+        maxD = 0
+
+        for i, s in enumerate(self.surround):
+            if s[1] > maxD and self.outsideRange(s[0]) and self.allPointsOnTrack(s[0]):
+                maxD = s[1]
+                self.maxDist = s
+
+    def outsideRange(self, point, radius=180):
+        x1, y1 = point
+        x, y = self.prev_pos
+        return math.pow(x1 - x, 2) + math.pow(y1 - y, 2) > math.pow(radius, 2)
+
+    def findPointInCircle(self, point, radius=5):
+        circle = []
+        y = -radius + point[1]
+        while y <= radius + + point[1]:
+            circle.append((int(point[0]), int(y)))
+            y += 1
+
+        sqRadius = radius * radius
+        h = radius
+        dx = 1
+        while dx <= radius:
+            while dx * dx + h * h > sqRadius and h > 0:
+                h -= 1
+
+            y = -h + point[1]
+            while y <= h + point[1]:
+                circle.append((int(point[0] + dx), int(y)))
+                circle.append((int(point[0] - dx), int(y)))
+                y += 1
+
+            dx += 1
+
+        return circle
+
+    def onTrack(self, circle):
+        for p in circle:
+            if p[0] >= 0 and p[0] <= WIN_WIDTH and p[1] >= 0 and p[1] <= WIN_HEIGHT:
+                if self.background.get_at(p) != self.track_color:
+                    return False
+
+        return True
+
+    def getNextPoint(self):
+        x = int(self.maxDist[0][0])
+        y = int(self.maxDist[0][1])
+
+        return (x, y)
+
+    def allPointsOnTrack(self, pos):
+        circle = self.findPointInCircle(pos)
+        circle_ok = self.onTrack(circle)
+
+        line = [(x, y) for (x, y) in self.BresenhamsLineAlgorithm(pos)]
+
+        line_ok = self.onTrack(line)
+
+        return circle_ok and line_ok
+
+    # Bresenhams line algorithm
+    def BresenhamsLineAlgorithm(self, pos):
+        x0, y0 = self.center_pos
+        x1, y1 = pos
+        x1 = int(x1)
+        y1 = int(y1)
+
+        deltax = x1-x0
+        if x1 == x0:
+            return (0, 0)
+        else:
+            dxsign = int(abs(deltax)/deltax)
+
+        deltay = y1-y0
+        if y1 == y0:
+            dysign = 0
+        else:
+            dysign = int(abs(deltay)/deltay)
+
+        if deltay == deltax or deltax == 0:
+            deltaerr = 0
+        else:
+            deltaerr = abs(deltay/deltax)
+
+        error = 0
+        y = y0
+        for x in range(x0, x1, dxsign):
+            yield x, y
+            error = error + deltaerr
+            while error >= 0.5:
+                y += dysign
+                error -= 1
+        yield x1, y1
 
     def check_surroundings(self, degree):
         len = 0
         x = self.pos[0] + math.cos(math.radians(360 - degree)) * len
         y = self.pos[1] + math.sin(math.radians(360 - degree)) * len
+        onScreen = True
 
-        while self.background.get_at((int(x), int(y))) == self.track_color and len < 200:
+        while onScreen and self.background.get_at((int(x), int(y))) == self.track_color and len < RADIUS_LENGTH:
             len += 1
             x = self.pos[0] + math.cos(math.radians(360 - degree)) * len
             y = self.pos[1] + math.sin(math.radians(360 - degree)) * len
+
+            if x >= 0 and x <= WIN_WIDTH and y >= 0 and y <= WIN_HEIGHT:
+                onScreen = True
+            else:
+                onScreen = False
 
         dist = math.sqrt(
             math.pow(x - self.pos[0], 2) + math.pow(y - self.pos[1], 2))
@@ -211,10 +393,23 @@ class Gate:
         return dist
 
     def draw(self, win):
-        pygame.draw.circle(win, (255, 0, 0), self.center_pos, 5)
+        # pygame.draw.circle(win, (255, 0, 0), self.center_pos, 5)
+        lineColor = (255, 0, 0)
+        if self.finished == True:
+            lineColor = (255, 0, 255)
+
         for s in self.surround:
-            if s == self.minDist or s == self.maxDist:
-                pygame.draw.line(win, (255, 0, 0), self.center_pos, s[0])
+            # if s == self.maxDist and not self.finished:
+            #     pygame.draw.line(win, lineColor, self.center_pos, s[0])
+
+            if s == self.minDist or s == self.minDist180:
+                pygame.draw.line(win, lineColor, self.center_pos, s[0])
+
+        if self.finished == True:
+            pygame.draw.line(win, (255, 255, 0), self.center_pos, self.meta)
+
+    def getGate(self):
+        return (self.minDist[0], self.minDist180[0])
 
 
 def main():
@@ -223,9 +418,13 @@ def main():
 
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     background = BG_IMG.convert()
+
+    gatesGen = GatesGenerator(background, (700, 825), 100, 30)
+    gatesGen.generate()
+    toPass = gatesGen.getGatesToPass()
+
     cars_img = CARS.convert_alpha()
-    cars = [Car(cars_img, 19)]
-    gates = [Gate((900, 825), background)]
+    cars = [Car(cars_img, 19, toPass)]
 
     while run:
         clock.tick(30)
@@ -252,7 +451,7 @@ def main():
         for car in cars:
             car.update()
 
-        draw_window(win, BG_IMG, cars, gates)
+        draw_window(win, BG_IMG, cars, gatesGen.getAllGates())
 
 
 def draw_window(win, BG, cars, gates=[]):
@@ -275,6 +474,7 @@ def eval_genomes(genomes, config):
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     background = BG_IMG.convert()
     cars_img = CARS.convert_alpha()
+
     run = True
     clock = pygame.time.Clock()
 
@@ -326,7 +526,7 @@ def eval_genomes(genomes, config):
             if car.speed > 10:
                 ge[index].fitness += 0.1
 
-        draw_window(win, BG_IMG, cars)
+        draw_window(win, background, cars)
 
 
 def run(config_path):
